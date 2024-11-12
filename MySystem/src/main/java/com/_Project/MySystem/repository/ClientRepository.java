9aeb68d1f72424f5b5f9fd4b3cb13ead61e382aa
JonathanDannessa <132615674+JonathanDannessa@users.noreply.github.com>
2024-11-11 21:15:07 -0500

    Delete MySystem directory

--- MySystem/src/main/java/com/_Project/MySystem/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com._Project.MySystem.repository;
-
-import com._Project.MySystem.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
--- MySystem/src/main/java/com/_Project/MySystem/repository/OfferingRepository.java
+++
@@ -1,9 +0,0 @@
-package com._Project.MySystem.repository;
-
-import com._Project.MySystem.model.Offering;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OfferingRepository extends JpaRepository<Offering, Long> {
-}
